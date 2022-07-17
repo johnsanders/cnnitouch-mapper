c57f99794573b110cfb6cc59defdbfae6b3f9b6f
John Sanders <john.sanders@warnermedia.com>
2022-07-17 09:51:23 +0100

    Update easing

--- src/components/map/animator/config.ts
+++ src/components/map/animator/config.ts
@@ -1,6 +1,6 @@
 import { Easing } from 'remotion';
 
-export const easing = Easing.bezier(0.21, 0.84, 0, 1);
+export const easing = Easing.out(Easing.exp);
 export const hiliteLabelThreshold = 0.0075;
 export const hiliteFadeOutThreshold = 0.8;
 export const fadeDuration = 15;
